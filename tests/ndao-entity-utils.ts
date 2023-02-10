890d62398c6c018a0cfdb69bfe6531193cd6347f
Pedro Yan Ornelas <pedroyane@gmail.com>
2023-02-09 21:22:45 -0300

    Stable point

--- tests/ndao-entity-utils.ts
+++ tests/ndao-entity-utils.ts
@@ -1,6 +1,29 @@
-import { Address, BigInt, Bytes, ethereum } from '@graphprotocol/graph-ts'
+import { Address, BigInt, Bytes, ethereum, log } from '@graphprotocol/graph-ts'
 import { EntityDonationReceived } from '../generated/templates/NdaoEntity/NdaoEntity'
 import { newMockEvent } from 'matchstick-as'
+
+export const DEFAULT_DONOR_ADDRESS = Address.fromString('0xe5Ce376f2904C780E6F006213719B38b73E286D7')
+export const DEFAULT_ENTITY_ADDRESS = Address.fromString('0xDf6b465463eA501cAccBcdf895AaDEfc5726FbF0')
+export const TOKEN_IN = Address.fromString('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
+export const DEFAULT_TOKEN_IN = BigInt.fromI64(10 ** 18)
+
+export function createDefaultDonationEvent(
+  to: Address,
+  amountReceived: u64,
+  blockNumber: i32 = 1,
+): EntityDonationReceived {
+  const amountInBi = BigInt.fromU64(amountReceived)
+  const fee = amountInBi.times(BigInt.fromI32(5)).div(BigInt.fromI32(1000)) // 0.5% fee
+  return createEntityDonationReceivedEvent(
+    DEFAULT_DONOR_ADDRESS,
+    to,
+    TOKEN_IN,
+    DEFAULT_TOKEN_IN,
+    amountInBi,
+    fee,
+    blockNumber,
+  )
+}
 
 export function createEntityDonationReceivedEvent(
   from: Address,
@@ -12,7 +35,7 @@ export function createEntityDonationReceivedEvent(
   blockNumber: i32 = 1,
 ): EntityDonationReceived {
   const donationEvent = changetype<EntityDonationReceived>(newMockEvent())
-  // donationEvent.block.number = BigInt.fromI32(blockNumber)
+  donationEvent.block.number = BigInt.fromI32(blockNumber)
 
   donationEvent.parameters = []
   donationEvent.parameters.push(new ethereum.EventParam('from', ethereum.Value.fromAddress(from)))
