5074daf02a78bac675ba7671627f1b863adbb65b
Pedro Yan Ornelas <pedroyane@gmail.com>
2023-02-13 18:20:45 -0300

    Added balance reconciled v1 migration test

--- tests/migration-detection.test.ts
+++ tests/migration-detection.test.ts
@@ -12,6 +12,7 @@ import {
   mockOrgId,
   createDefaultValuePaidOutEvent,
   EXTERNAL_ADDRESS,
+  createDefaultBalanceReconciledEvent,
 } from './utils/ndao-entity'
 import { Address, BigInt, log } from '@graphprotocol/graph-ts'
 import { EntityDonationReceived } from '../generated/templates/NdaoEntity/NdaoEntity'
@@ -19,6 +20,7 @@ import { handleEntityDeployed } from '../src/mappings/org-fund-factory'
 import { createEntityDeployedEvent } from './utils/org-fund-factory'
 import { OnChainNdaoEntityType } from '../src/utils/on-chain-entity-type'
 import {
+  handleEntityBalanceReconciled,
   handleEntityDonationReceived,
   handleEntityValuePaidOut,
   handleEntityValueTransferred,
@@ -193,7 +195,6 @@ describe('Migration Detection Tests', () => {
       handleEntityDeployed(fundDeployed)
     })
 
-    // TODO: Use this test below as a blueprint for payout indexing tests
     test('it should correctly index transfer event from an entity with V1 Assets (zero final balance)', () => {
       // ------ Act -------
       // Block 1:
@@ -515,6 +516,49 @@ describe('Migration Detection Tests', () => {
       assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcPaidOutFees)
       assert.booleanEquals(true, entity.initialized)
     })
+  })
+
+  test('it should correctly index balance reconciled with V1 Assets', () => {
+    // ------ Act -------
+    // Block 1:
+    // - 1 donations of 200 USD
+    // - Fees = 1.0 USD (0.5%)
+    // - V1 Migrated Assets = 1200 USD
+    // - Balance at the end of block: 1399,00 USD
+    mockBalance(DEFAULT_ORG_ADDRESS, 1_399_000_000)
+    handleEntityBalanceReconciled(createDefaultBalanceReconciledEvent(DEFAULT_ORG_ADDRESS, 200_000_000))
+
+    // Block 2:
+    // - 1 donation of 150 USD.
+    // - Fees = 0.75 USD (0.5%)
+    // - Balance at the end of block: 1548.25 USD
+    mockBalance(DEFAULT_ORG_ADDRESS, 1_548_250_000)
+    handleEntityDonationReceived(createDefaultDonationEvent(DEFAULT_ORG_ADDRESS, 150_000_000, 2))
+    const entity = NdaoEntity.load(DEFAULT_ORG_ADDRESS)
+
+    // ------ Assert ------
+    if (!entity) throw new Error('Entity not found in store')
+
+    assert.bigIntEquals(BigInt.fromI32(1_548_250_000), entity.recognizedUsdcBalance)
+    assert.bigIntEquals(BigInt.fromI32(0), entity.investmentBalance)
+    assert.bigIntEquals(BigInt.fromI32(348_250_000), entity.totalUsdcDonationsReceived)
+    assert.bigIntEquals(BigInt.fromI32(1_750_000), entity.totalUsdcDonationFees)
+    assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcGrantsReceived)
+    assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcGrantInFees)
+    assert.bigIntEquals(BigInt.fromI32(348_250_000), entity.totalUsdcContributionsReceived)
+    assert.bigIntEquals(BigInt.fromI32(1_750_000), entity.totalUsdcContributionFees)
+    assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcTransfersReceived)
+    assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcTransferInFees)
+    assert.bigIntEquals(BigInt.fromI32(1200_000_000), entity.totalUsdcMigrated)
+    assert.bigIntEquals(BigInt.fromI32(1_548_250_000), entity.totalUsdcReceived)
+    assert.bigIntEquals(BigInt.fromI32(1_750_000), entity.totalUsdcReceivedFees)
+    assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcGrantedOut)
+    assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcGrantedOutFees)
+    assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcTransferredOut)
+    assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcTransferredOutFees)
+    assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcPaidOut)
+    assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcPaidOutFees)
+    assert.booleanEquals(true, entity.initialized)
   })
 
   test('it should correctly index payout event from an entity with V1 Assets', () => {
