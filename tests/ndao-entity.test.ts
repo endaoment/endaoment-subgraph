30ed78d298dc9c64028e6013334b32e5833da3d3
Pedro Yan Ornelas <pedroyane@gmail.com>
2023-02-13 21:27:13 -0300

    Added partial redemption test

--- tests/ndao-entity.test.ts
+++ tests/ndao-entity.test.ts
@@ -598,6 +598,50 @@ describe('NdaoEntity Tests', () => {
     })
 
     // TODO: it should correctly index a partial redemption (50%)
+    test('it should correctly index a partial redemption (50%)', () => {
+      // ------ Arrange -------
+      const investedAmount: u64 = 400_000_000
+      const shares: u64 = investedAmount * 10
+
+      // ------ Act -------
+      mockBalance(DEFAULT_ENTITY_ADDRESS, 400_000_000)
+      handleEntityDeposit(createEntityDepositEvent(DEFAULT_ENTITY_ADDRESS, PORTFOLIO_1_ADDRESS, investedAmount, shares))
+      handleEntityRedeem(
+        createEntityRedeemEvent(DEFAULT_ENTITY_ADDRESS, PORTFOLIO_1_ADDRESS, shares / 2, investedAmount),
+      )
+
+      // ------ Assert ------
+      const entity = NdaoEntity.load(DEFAULT_ENTITY_ADDRESS)
+      if (!entity) throw new Error('Entity not found in store')
+
+      assert.bigIntEquals(BigInt.fromI32(400_000_000), entity.recognizedUsdcBalance)
+      assert.bigIntEquals(BigInt.fromU64(200_000_000), entity.investedUsdc)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcDonationsReceived)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcDonationFees)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcGrantsReceived)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcGrantInFees)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcContributionsReceived)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcContributionFees)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcTransfersReceived)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcTransferInFees)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcMigrated)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcReceived)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcReceivedFees)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcGrantedOut)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcGrantedOutFees)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcTransferredOut)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcTransferredOutFees)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcPaidOut)
+      assert.bigIntEquals(BigInt.fromI32(0), entity.totalUsdcPaidOutFees)
+
+      const position = PortfolioPosition.load(`${PORTFOLIO_1_ADDRESS.toHex()}|${DEFAULT_ENTITY_ADDRESS.toHex()}`)
+      if (!position) throw new Error('Portfolio position not found in store')
+
+      assert.bytesEquals(entity.id, position.entity)
+      assert.bytesEquals(PORTFOLIO_1_ADDRESS, position.portfolio)
+      assert.bigIntEquals(BigInt.fromU64(shares / 2), position.shares)
+      assert.bigIntEquals(BigInt.fromU64(investedAmount / 2), position.investedUsdc)
+    })
 
     // TODO: it should correctly index a partial redemption (25%)
 
