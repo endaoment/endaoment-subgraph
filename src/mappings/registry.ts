8c2432fef4fa22119c897c1478f9e1bd52b5a49a
Pedro Yan Ornelas <pedroyane@gmail.com>
2023-02-15 22:10:10 -0300

    Added registry approvals

--- src/mappings/registry.ts
+++ src/mappings/registry.ts
@@ -7,10 +7,31 @@ import {
   SwapWrapperStatusSet,
   UserRoleUpdated,
 } from '../../generated/Registry/Registry'
+import { resolveRegistry } from '../utils/registry-utils'
 
-export function handleFactoryApprovalSet(event: FactoryApprovalSet): void {}
+export function handleFactoryApprovalSet(event: FactoryApprovalSet): void {
+  const registry = resolveRegistry(event.address)
 
-export function handleSwapWrapperStatusSet(event: SwapWrapperStatusSet): void {}
+  if (event.params.isApproved) {
+    registry.entityFactories.push(event.params.factory)
+  } else {
+    registry.entityFactories = registry.entityFactories.filter((factory) => factory != event.params.factory)
+  }
+
+  registry.save()
+}
+
+export function handleSwapWrapperStatusSet(event: SwapWrapperStatusSet): void {
+  const registry = resolveRegistry(event.address)
+
+  if (event.params.isSupported) {
+    registry.swapWrappers.push(event.params.swapWrapper)
+  } else {
+    registry.swapWrappers = registry.swapWrappers.filter((swapWrapper) => swapWrapper != event.params.swapWrapper)
+  }
+
+  registry.save()
+}
 
 export function handlePortfolioStatusSet(event: PortfolioStatusSet): void {}
 
